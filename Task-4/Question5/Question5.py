dda068ee928aa1e1a01ff3c3fb69ede04144ca71
KsHarshil <99633170+KsHarshil@users.noreply.github.com>
2022-02-23 22:42:04 +0530

    Add files via upload

---
+++ Task-4/Question5/Question5.py
@@ -0,0 +1,19 @@
+def star_triangle(n):
+     
+    m = n - 1
+    
+    for i in range(0, n):
+     
+        for j in range(0, m):
+            print(end=" ")
+     
+        m = m - 1
+     
+        for j in range(0, i+1):
+         
+            print("* ", end="")
+     
+        print("\r")
+        
+n = 5
+star_triangle(n)
